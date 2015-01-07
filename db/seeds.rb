c227816fcbe360d3ab8e22295acffbd9b84c12de
timmcanty <timmcanty@gmail.com>
2015-01-07 17:55:06 -0500

    Adds User show functionality

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,7 @@
 Film.create(title: 'Guardians of the Galaxy',
   imdb_url: 'http://www.imdb.com/title/tt2015381/',
   description: 'A group of intergalactic criminals are forced to work together to stop a fanatical warrior from taking control of the universe.')
+
+Film.create(title: 'The Muppets',
+  imdb_url: 'http://www.imdb.com/title/tt1204342/',
+  description: 'A Muppet fanatic with some help from his two human compatriots must regroup the Muppet gang to stop a avaricious oil mogul from taking down one of their precious life-longing treasures.')

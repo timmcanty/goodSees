1de798628ec1303af6cbb116e67695c8b525183f
timmcanty <timmcanty@gmail.com>
2015-01-08 18:11:42 -0500

    Fixes 500 error for not signed in user

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,6 +1,10 @@
 module Api
   class UsersController < ApiController
 
+    def index
+      render :index
+    end
+
     def show
       @user = User.includes(:ratings, reels: :films).find(params[:id])
       render :show
---
+++ app/views/api/users/index.json.jbuilder
@@ -0,0 +1,1 @@
+'todo: add users index'
